f746c4c53870d8f6a83974db49126b2f0d902ba3
Khizar Iqbal <khizar.i0066@gmail.com>
2023-08-21 16:17:36 +0100

    className being used

--- client/src/components/NumberBubble.jsx
+++ client/src/components/NumberBubble.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 function NumberBubble() {
 	return (
-		<div class="aspect-square border-dashed border-black border-4 rounded-full w-24 h-24 sm:w-28 sm:h-28"></div>
+		<div className="aspect-square border-dashed border-black border-4 rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36"></div>
 	);
 }
 
