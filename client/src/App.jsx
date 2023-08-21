6949d11b18bf916a2570160e37b96ced603596e1
Khizar Iqbal <khizar.i0066@gmail.com>
2023-08-21 16:10:07 +0100

    deez

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
-import NumberBubble from './components/NumberBubble.jsx'
-import './App.css'
-import Target from './components/Target.jsx'
+import NumberBubble from "./components/NumberBubble.jsx";
+import "./App.css";
+import Target from "./components/Target.jsx";
 
 function App() {
-  return (
-    <>
-      <Target />
-      <div class="container mx-auto p-4">
-        <div class="grid grid-cols-3 grid-rows-2 gap-4">
-          <NumberBubble />
-          <NumberBubble />
-          <NumberBubble />
-          <NumberBubble />
-          <NumberBubble />
-          <NumberBubble />
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Target />
+			<div className="container mx-auto p-4">
+				<div className="grid grid-cols-3 grid-rows-2 gap-4 min-w-max">
+					<NumberBubble />
+					<NumberBubble />
+					<NumberBubble />
+					<NumberBubble />
+					<NumberBubble />
+					<NumberBubble />
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- client/src/components/NumberBubble.jsx
+++ client/src/components/NumberBubble.jsx
@@ -1,11 +1,9 @@
-import '../App.css'
+import "../App.css";
 
 function NumberBubble() {
-  return (
-    <div class="aspect-square border-dashed border-black border-4 rounded-full h-full w-full">
-      
-    </div>
-  )
+	return (
+		<div class="aspect-square border-dashed border-black border-4 rounded-full w-24 h-24 sm:w-28 sm:h-28"></div>
+	);
 }
 
-export default NumberBubble
+export default NumberBubble;
