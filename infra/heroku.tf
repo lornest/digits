9d318679bf010035aef2bb81b79abf23ae471f2d
Lorne Stalker <lorne.stalker@gmail.com>
2023-08-28 09:06:12 +0100

    moving API key to provider block

--- infra/heroku.tf
+++ infra/heroku.tf
@@ -1,3 +1,7 @@
+provider "heroku" {
+  api_key = var.heroku_api_key
+}
+
 resource "heroku_app" "digits-server" {
   name   = "digits-server"
   region = "dublin"
--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,6 @@ terraform {
     heroku = {
       source  = "heroku/heroku"
       version = "~> 5.0"
-      api_key = var.heroku_api_key
     }
   }
 }
