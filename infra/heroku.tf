5698c4e9a199b4c3ee21eca7353fa839f9c1a360
Lorne Stalker <lorne.stalker@gmail.com>
2023-08-28 08:57:29 +0100

    testing out Heroku provider and TF cloud

---
+++ infra/heroku.tf
@@ -0,0 +1,4 @@
+resource "heroku_app" "digits-server" {
+  name   = "digits-server"
+  region = "dublin"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+variable "heroku_api_key" {
+  type        = string
+  sensitive   = true
+  description = "API key for Heroku"
+}
