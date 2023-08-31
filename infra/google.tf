9b4e17fc7487589c9c1b0456b12582f22ac6403b
Lorne Stalker <lorne.stalker@gmail.com>
2023-08-31 18:56:17 +0100

    allowing all users HTTP access to test

--- infra/google.tf
+++ infra/google.tf
@@ -3,6 +3,13 @@ provider "google" {
   region  = "europe-west1"
 }
 
+# Enable the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+
+  disable_on_destroy = true
+}
+
 resource "google_cloud_run_v2_service" "digits-server-test" {
   name     = "digits-server-test"
   location = "europe-west1"
@@ -13,4 +20,14 @@ resource "google_cloud_run_v2_service" "digits-server-test" {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
   }
+
+  depends_on = [google_project_service.run_api]
 }
+
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_v2_service.digits-server-test.name
+  location = google_cloud_run_v2_service.digits-server-test.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
