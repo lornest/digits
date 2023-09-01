714bed8a9233aaa2dd51c38d865068b4bbcd852d
Lorne Stalker <lorne.stalker@gmail.com>
2023-09-01 09:44:28 +0100

    commenting out file for now

--- infra/gcr.tf
+++ infra/gcr.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "container-images" {
+/* resource "google_storage_bucket" "container-images" {
   name          = "container-images"
   location      = "europe-west2"
   force_destroy = true
@@ -8,4 +8,4 @@ resource "google_storage_bucket" "container-images" {
 
 resource "google_container_registry" "container-images" {
   depends_on = [google_storage_bucket.container-images, google_project_service.cloudresourcemanager_api]
-}
+} */
